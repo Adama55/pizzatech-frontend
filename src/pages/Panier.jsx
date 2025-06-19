8e951ccc35b8a6d7c62d88004d700a4ba4bb4ce5
Adama55 <mafahfof55@gmail.com>
2025-06-19 23:19:31 +0200

    add pannier

--- src/pages/Panier.jsx
+++ src/pages/Panier.jsx
@@ -13,7 +13,7 @@ export default function Panier() {
 
                     </Col>
                 </Row>
-                {/* Ajoute d'autres sections ici */}
+
             </Container>
         </div>
     );
