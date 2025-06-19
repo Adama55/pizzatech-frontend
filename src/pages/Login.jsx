7d1454b093fd2bd10b8a9f26a0974b63764d340a
Adama55 <mafahfof55@gmail.com>
2025-06-19 23:19:44 +0200

    add login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+// src/pages/Login.jsx
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div className="container mt-5">
+            <h2>Connexion</h2>
+            {/* Formulaire à venir */}
+        </div>
+    );
+}
+
+
