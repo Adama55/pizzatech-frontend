85be5513fc1bc8ff98eaa024b7d09f461e63f8f0
Adama55 <mafahfof55@gmail.com>
2025-06-24 23:20:01 +0200

    add admin

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Layout from "../Layout.jsx";
+
+export default function Admin() {
+    return (
+        <div>
+
+            <Container className="mt-4">
+                <Row>
+                    <Col md={12}>
+                        <h1>admin</h1>
+
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
+    );
+}
